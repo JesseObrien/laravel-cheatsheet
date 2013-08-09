61903a1955b9e7a12cc571d62e02370dde5a5002
Jesse O'Brien <jesse@jesse-obrien.ca>
2013-08-09 14:32:53 -0400

    Loads of updates.
    Added helpers and HTML bindings in regards to Issue #2.
    
    Also swapped the auto-running prettify js link with the manual one.
    If it loads automatically you can't touch the dom properly.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,10 @@
 $('.comments-toggle').on('click', function(event){
 	$('span.com').toggle();
 });
+
+// Call prettprint manually
+prettyPrint();
+
+// Shine up the HTML:: calls
+var $elements = $("span.pln:contains('HTML')").removeClass('pln').addClass('typ');
+
